2ca3291230cc7f52f3f145283be09962c04d11d4
stonefullstm <stonefull.stm@gmail.com>
2023-04-13 14:58:50 -0300

    feat: Requisito 3 - Implementar a função scrape_next_page_link para retornar o link para a próxima página de notícias

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -32,7 +32,14 @@ def scrape_updates(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    soup = BeautifulSoup(html_content, "html.parser")
+    try:
+        return soup.find(
+            "a",
+            {"class": "next page-numbers"},
+        )["href"]
+    except TypeError:
+        return None
 
 
 # Requisito 4
