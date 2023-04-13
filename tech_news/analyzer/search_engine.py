cc26c0a4a7a84bda99981a6882b01fa8a79b38be
stonefullstm <stonefull.stm@gmail.com>
2023-04-13 19:14:31 -0300

    feat: Requisito 9 - Implementar função search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+import datetime
 
 
 # Requisito 7
@@ -9,7 +10,14 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        # date = datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
+        fdate = datetime.datetime.fromisoformat(date).strftime("%d/%m/%Y")
+        result = search_news({"timestamp": fdate})
+    except ValueError:
+        raise ValueError("Data inválida")
+    else:
+        return [(news["title"], news["url"]) for news in result]
 
 
 # Requisito 9
