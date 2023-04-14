171b7e4dc9988945675a9a6703690861c2a1d36f
stonefullstm <stonefull.stm@gmail.com>
2023-04-14 16:16:32 -0300

    feat: Requisito 10 - Implementar a função top_5_categories para retornar as 5 categorias com maior ocorrência

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,14 @@
+from tech_news.database import search_news
+from collections import Counter
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    all_news = search_news({})
+    categories = Counter(news["category"] for news in all_news)
+    # Esta ordenação em duas passadas só funciona porque o sorted()
+    # é estável: não altera a ordem relativa dos elementos
+    # comparados como iguais
+    first_order = sorted(categories.items(), key=lambda item: item[0])
+    second_order = sorted(first_order, key=lambda item: item[1], reverse=True)
+    return [item[0] for item in second_order][:5]
